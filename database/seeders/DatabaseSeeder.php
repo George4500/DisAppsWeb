3d1778664ccdfa4a908f4527e02345be32c79f4e
George4500 <128199467+George4500@users.noreply.github.com>
2025-03-04 19:56:28 -0600

    Eliminar modelo y seeder de Superhéroes, y agregar migraciones para Universos, Géneros y Superhéroes.

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,15 +15,11 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-            
-            $this->call([
-                UniverseSeeder::class,
-                GenderSeeder::class,
-                SuperheroSeeder::class, 
-            ])
+
+        $this->call([
+            UniverseSeeder::class,
+            GenderSeeder::class,
+            SuperheroSeeder::class, 
         ]);
     }
 }
