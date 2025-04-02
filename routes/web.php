a67a42037503120bc93e924f1696d8899706978e
George4500 <128199467+George4500@users.noreply.github.com>
2025-04-01 22:25:45 -0600

    Agregar componentes de vista y controladores de autenticación para el manejo de usuarios y verificación de correo electrónico

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
-use App\Models\Universe;
-USE App\Http\Controllers\GenderController;
-use App\Http\Controllers\UniverseController;
 
 Route::get('/', function () {
-    echo 'Hello World, this is my first Laravel project!';
-    // dump(Universe::all());
-    dump(Universe::all());
+    return view('welcome');
 });
 
-Route::get("/gender", [GenderController::class, "index"]);
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-/* Route::get("/universes", [UniverseController::class, "index"]);
-Route::get("/universes/create", [UniverseController::class, "create"]); */
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-Route::resource('universes', UniverseController::class);
-Route::resource('Superheroes', SuperheroController::class);
+require __DIR__.'/auth.php';
